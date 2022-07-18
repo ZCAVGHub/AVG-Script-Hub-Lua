c80015539b7493e65c70da7a768cef82947a413a
ZCAVGHub <109470610+ZCAVGHub@users.noreply.github.com>
2022-07-18 18:45:52 +0700

    Update HeadLock.lua

--- HeadLock.lua
+++ HeadLock.lua
@@ -1,26 +1,62 @@
+--[[
+███████████████████████████████████████████████████████████████████████╗
+██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██║
+██░░░█████╗░░░░░██╗░░░██╗░░░░░██████╗░░░░░██╗░░██╗██╗░░░██╗██████╗░░░██║
+██░░██╔══██╗░░░░██║░░░██║░░░░██╔════╝░░░░░██║░░██║██║░░░██║██╔══██╗░░██║
+██░░███████║░░░░╚██╗░██╔╝░░░░██║░░██╗░░░░░███████║██║░░░██║██████╦╝░░██║
+██░░██╔══██║░░░░░╚████╔╝░░░░░██║░░╚██╗░░░░██╔══██║██║░░░██║██╔══██╗░░██║
+██░░██║░░██║░██╗░░╚██╔╝░░██╗░╚██████╔╝░░░░██║░░██║╚██████╔╝██████╦╝░░██║
+██░░╚═╝░░╚═╝░╚═╝░░░╚═╝░░░╚═╝░░╚═════╝░░░░░╚═╝░░╚═╝░╚═════╝░╚═════╝░░░██║
+██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██║
+███████████████████████████████████████████████████████████████████████║
+╔══════════════════════════════════════════════════════════════════════╝
+║██████████████████████████████████████████████████████████████████████
+║██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
+║██░░░░░░░░░██████╗░░█████╗░██████╗░██╗░░░░░░█████╗░██╗░░██╗░░░░░░░░░██
+║██░░░░░░░░░██╔══██╗██╔══██╗██╔══██╗██║░░░░░██╔══██╗╚██╗██╔╝░░░░░░░░░██
+║██░░░░░░░░░██████╔╝██║░░██║██████╦╝██║░░░░░██║░░██║░╚███╔╝░░░░░░░░░░██
+║██░░░░░░░░░██╔══██╗██║░░██║██╔══██╗██║░░░░░██║░░██║░██╔██╗░░░░░░░░░░██
+║██░░░░░░░░░██║░░██║╚█████╔╝██████╦╝███████╗╚█████╔╝██╔╝╚██╗░░░░░░░░░██
+║██░░░░░░░░░╚═╝░░╚═╝░╚════╝░╚═════╝░╚══════╝░╚════╝░╚═╝░░╚═╝░░░░░░░░░██
+║██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
+║██████████████████████████████████████████████████████████████████████
+╚════════════════════════════════════════════════════════════════════╝
+AVG Script By ZC ZeroCash
+]]--
 local Player = game.Players.LocalPlayer
 local CurrentCamera = game.Workspace.CurrentCamera
 local Mouse = Player:GetMouse()
+
 function lookAt(humpos, target)
-local ForwardVector = (target - humpos).Unit
-local UpVector = Vector3.new(0, .5, 0)
-local RightVector = ForwardVector:Cross(UpVector)
-local UpVector2 = RightVector:Cross(ForwardVector)
-return CFrame.fromMatrix(humpos, RightVector, UpVector)
+	local ForwardVector = (target - humpos).Unit
+	local UpVector = Vector3.new(0, 1, 0)
+	local RightVector = ForwardVector:Cross(UpVector)
+	local UpVector2 = RightVector:Cross(ForwardVector)
+
+	return CFrame.fromMatrix(humpos, RightVector, UpVector)
 end
 local db = true
+local db2 = false
+
 Mouse.Button1Down:Connect(function()
-db = false
-if db == false then
-local Character = game:GetService("Players").LocalPlayer.Character
-local region = Region3.new(Mouse.Hit.p - Vector3.new(.5,.5,.5),Mouse.Hit.p + Vector3.new(.5,.5,.5))
-local inregion = game:GetService("Workspace"):FindPartsInRegion3(region,Character.HumanoidRootPart,math.huge)
-for i, hit in pairs(inregion) do
-if hit.Parent:FindFirstChild("Humanoid") and db == false then
-db = true
-CurrentCamera.CFrame = lookAt(Character.HumanoidRootPart.Position, hit.Parent.HumanoidRootPart.Position)
-wait(0.000001)
-end
-end
-end
+	db2 = false
+	db = false
+	if db == false and db2 == false then
+		local Character = game:GetService("Players").LocalPlayer.Character
+		local region = Region3.new(Mouse.Hit.p - Vector3.new(.5,.5,.5),Mouse.Hit.p + Vector3.new(.5,.5,.5))
+		local inregion = game:GetService("Workspace"):FindPartsInRegion3(region,Character.HumanoidRootPart,math.huge)
+
+		for i, hit in pairs(inregion) do
+			if hit.Parent:FindFirstChild("Humanoid") and db2 == false then
+				db2 = true
+				db = true
+				CurrentCamera.CFrame = lookAt(Character.HumanoidRootPart.Position, hit.Parent.HumanoidRootPart.Position)
+				wait(0.000001)
+			end
+		end
+	end
+end)
+Mouse.Button1Up:Connect(function()
+	db = true
+	db2 = false
 end)
