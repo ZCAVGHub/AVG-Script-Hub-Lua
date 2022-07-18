d27a10c89a02b06dba94d71a76d4cb19df4e620c
ZCAVGHub <109470610+ZCAVGHub@users.noreply.github.com>
2022-07-18 20:03:04 +0700

    Update HeadLock.lua

--- HeadLock.lua
+++ HeadLock.lua
@@ -21,7 +21,6 @@
 ║██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
 ║██████████████████████████████████████████████████████████████████████
 ╚════════════════════════════════════════════════════════════════════╝
-AVG Script By ZC ZeroCash
 ]]--
 local Player = game.Players.LocalPlayer
 local CurrentCamera = game.Workspace.CurrentCamera
@@ -29,34 +28,26 @@ local Mouse = Player:GetMouse()
 
 function lookAt(humpos, target)
 	local ForwardVector = (target - humpos).Unit
-	local UpVector = Vector3.new(0, 1, 0)
-	local RightVector = ForwardVector:Cross(UpVector)
+	local RightVector = ForwardVector:Cross(Vector3.new(0, 1, 0))
 	local UpVector2 = RightVector:Cross(ForwardVector)
 
-	return CFrame.fromMatrix(humpos, RightVector, UpVector)
+	return CFrame.fromMatrix(humpos, RightVector, UpVector2)
 end
 local db = true
-local db2 = false
 
 Mouse.Button1Down:Connect(function()
-	db2 = false
 	db = false
-	if db == false and db2 == false then
+	if db == false then
 		local Character = game:GetService("Players").LocalPlayer.Character
-		local region = Region3.new(Mouse.Hit.p - Vector3.new(.5,.5,.5),Mouse.Hit.p + Vector3.new(.5,.5,.5))
+		local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart")
+		local region = Region3.new(Mouse.Hit.p - Vector3.new(.2,.2,.2),Mouse.Hit.p + Vector3.new(.2,.2,.2))
 		local inregion = game:GetService("Workspace"):FindPartsInRegion3(region,Character.HumanoidRootPart,math.huge)
-
 		for i, hit in pairs(inregion) do
-			if hit.Parent:FindFirstChild("Humanoid") and db2 == false then
-				db2 = true
+			if hit.Parent:FindFirstChild("Humanoid") and db == false then
 				db = true
-				CurrentCamera.CFrame = lookAt(Character.HumanoidRootPart.Position, hit.Parent.HumanoidRootPart.Position)
-				wait(0.000001)
+				CurrentCamera.CFrame = lookAt(HumanoidRootPart.Position, hit.Parent:FindFirstChild("Head").Position+Vector3.new(0,-1,0))
+				wait()
 			end
 		end
 	end
-end)
-Mouse.Button1Up:Connect(function()
-	db = true
-	db2 = false
 end)
